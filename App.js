cfa4b903182ec4fce7c4c302fd130713b4ba9c41
andrewdmo <andrewdmo@gmail.com>
2020-10-06 15:05:27 -0400

    -pre ampflify

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Dimensions} from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <MapView
+            style={styles.mapStyle}
+            initialRegion={{
+              latitude: 35.594722,
+              longitude: -82.554167,
+              latitudeDelta: 1,      // 1 deg lat ~ 70 mi
+              longitudeDelta: 1
+            }}
+        />
+        <StatusBar style="auto"/>
+      </View>
   );
 }
 
@@ -18,4 +28,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mapStyle: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
 });
